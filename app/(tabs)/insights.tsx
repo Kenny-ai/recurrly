a47ac2ab84f5896e76e8a12927527a7a58837b34
Kenny <kennysoyoye07@gmail.com>
2026-04-10 21:49:24 +0100

    chore: implement fixes

--- app/(tabs)/insights.tsx
+++ app/(tabs)/insights.tsx
@@ -8,7 +8,7 @@ const SafeAreaView = styled(RNSafeAreaView);
 
 const Insights = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView className="flex-1 bg-background p-5">
       <Text>Insights</Text>
     </SafeAreaView>
   );
